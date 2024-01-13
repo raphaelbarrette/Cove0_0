524661e88d85122215b53a8118cf54f70e6aa6ac
benjy201 <labb1904@usherbrooke.ca>
2024-01-13 14:54:42 -0500

    adding radar scans

--- java/src/main/java/codes/blitz/game/bot/Bot.java
+++ java/src/main/java/codes/blitz/game/bot/Bot.java
@@ -6,6 +6,7 @@ import codes.blitz.game.message.game.*;
 import codes.blitz.game.message.game.actions.*;
 
 public class Bot {
+
     public Bot() {
         System.out.println("Initializing your super duper mega bot.");
     }
@@ -15,15 +16,14 @@ public class Bot {
      */
     public List<Action> getActions(GameMessage gameMessage) {
         List<Action> actions = new ArrayList<>();
-
         Ship myShip = gameMessage.ships().get(gameMessage.currentTeamId());
         List<String> otherShipsIds = new ArrayList<>(gameMessage.shipsPositions().keySet());
-        otherShipsIds.removeIf(shipId -> shipId == gameMessage.currentTeamId());
-
+        otherShipsIds.removeIf(shipId -> shipId.equals(gameMessage.currentTeamId()));
         // Find who's not doing anything and try to give them a job.
         List<Crewmate> idleCrewmates = new ArrayList<>(myShip.crew());
         idleCrewmates.removeIf(crewmate -> crewmate.currentStation() != null || crewmate.destination() != null);
         Map<String, String> crewmateLocations = new HashMap<>();
+
 
 
         for (Crewmate crewmate : idleCrewmates) {
@@ -76,15 +76,67 @@ public class Bot {
 
         List<RadarStation> operatedRadarStations = new ArrayList<>(myShip.stations().radars());
         operatedRadarStations.removeIf(radarStation -> radarStation.operator() == null);
-        for (RadarStation radarStation : operatedRadarStations) {
-            actions.add(new RadarScanAction(radarStation.id(), otherShipsIds.get(0)));
-        }
+//        for (RadarStation radarStation : operatedRadarStations) {
+//            actions.add(new RadarScanAction(radarStation.id(), otherShipsIds.get(0)));
+//        }
+
         List<Crewmate> Crewmates = new ArrayList<>(myShip.crew());
         repareShield(gameMessage, Crewmates, actions, myShip);
         // You can clearly do better than the random actions above. Have fun!!
         return actions;
     }
-    
+    public String radarScan(GameMessage gameMessage,List<String> otherShipsIds,List<Crewmate> crewmateList, List<Action> actions, Ship myShip,Map<String, String> crewmateLocations) {
+        boolean checkedPos1 = false;
+        boolean checkedPos2 = false;
+        boolean checkedPos3 = false;
+        int shortestDistance = 1000000;
+        StationDistance stationToMoveTo = null;
+        Crewmate crewToMove = null;
+        String targetId = null;
+        if (gameMessage.currentTickNumber() % 300 == 0) {
+            for (Crewmate crewmate : crewmateList) {
+                for (StationDistance stationDistance : crewmate.distanceFromStations().radars()) {
+                    if (stationDistance.distance() < shortestDistance) {
+                        stationToMoveTo = stationDistance;
+                        crewToMove = crewmate;
+                        System.out.println("Moving crewmate to radar station" + crewToMove.id() + " : " + stationToMoveTo.stationPosition() + "because need information");
+                    }
+                }
+            }
+            actions.add(new MoveCrewAction(crewToMove.id(), stationToMoveTo.stationPosition()));
+        }
+        if (crewmateLocations.containsKey(stationToMoveTo.stationId())) {
+            double health = 1000;
+
+            if (crewmateLocations.containsKey(stationToMoveTo.stationId()) && !checkedPos1) {
+                actions.add(new RadarScanAction(stationToMoveTo.stationId(), otherShipsIds.get(0)));
+                double health_1 = gameMessage.ships().get(1).currentHealth();
+                if (health_1<health){
+                    health = health_1;
+                    targetId = otherShipsIds.get(0);
+                }
+                checkedPos1=true;
+            } else if (crewmateLocations.containsKey(stationToMoveTo.stationId()) && !checkedPos2) {
+                actions.add(new RadarScanAction(stationToMoveTo.stationId(), otherShipsIds.get(1)));
+                double health_2 = gameMessage.ships().get(2).currentHealth();
+                if (health_2<health){
+                    health = health_2;
+                    targetId = otherShipsIds.get(1);
+                }
+                checkedPos2=true;
+            } else if (crewmateLocations.containsKey(stationToMoveTo.stationId()) && !checkedPos3) {
+                actions.add(new RadarScanAction(stationToMoveTo.stationId(), otherShipsIds.get(2)));
+                double health_3 = gameMessage.ships().get(3).currentHealth();
+                if (health_3<health){
+                    health = health_3;
+                    targetId=otherShipsIds.get(2);
+                }
+                checkedPos3=true;
+            }
+
+        }
+        return targetId;
+    }
     public void repareShield(GameMessage gameMessage, List<Crewmate> crewmateList, List<Action> actions, Ship myShip) {
         int shortestDistance = 1000000;
         StationDistance stationToMoveTo = null;
